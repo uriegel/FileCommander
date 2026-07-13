f9ea709aba43ec333b4153b04a0d4c683bc807db
Uwe Riegel <uriegel@hotmail.de>
2026-07-13 18:47:34 +0200

    Hamburger

--- FileCommander/MainWindow.xaml.cs
+++ FileCommander/MainWindow.xaml.cs
@@ -1,97 +1,67 @@
-using Microsoft.UI.Input;
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
+
 using System;
+
 using Windows.ApplicationModel;
-using Rect = Windows.Foundation.Rect;
 
 namespace FileCommander;
 
 public sealed partial class MainWindow : Window
 {
-    private AppWindow m_AppWindow;
-
     public MainWindow()
     {
         this.InitializeComponent();
 
         // Assumes "this" is a XAML Window. In projects that don't use 
         // WinUI 1.3 or later, use interop APIs to get the AppWindow.
-        m_AppWindow = this.AppWindow;
-        m_AppWindow.Changed += AppWindow_Changed;
+        AppWindow.Changed += AppWindow_Changed;
         Activated += MainWindow_Activated;
         AppTitleBar.SizeChanged += AppTitleBar_SizeChanged;
         AppTitleBar.Loaded += AppTitleBar_Loaded;
 
         ExtendsContentIntoTitleBar = true;
         if (ExtendsContentIntoTitleBar == true)
-        {
-            m_AppWindow.TitleBar.PreferredHeightOption = TitleBarHeightOption.Tall;
-        }
-        TitleBarTextBlock.Text = AppInfo.Current.DisplayInfo.DisplayName;
+            AppWindow.TitleBar.PreferredHeightOption = TitleBarHeightOption.Tall;
+        
+        // Laufzeit-Guard für API, die ab Windows 10.0.19041 verfügbar ist
+        string appDisplayName = "File Commander";
+        if (OperatingSystem.IsWindowsVersionAtLeast(10, 0, 19041))
+            appDisplayName = AppInfo.Current.DisplayInfo.DisplayName;
+        TitleBarTextBlock.Text = appDisplayName;
     }
 
-    private void AppTitleBar_Loaded(object sender, RoutedEventArgs e)
+    void AppTitleBar_Loaded(object sender, RoutedEventArgs e)
+    {
+        if (ExtendsContentIntoTitleBar == true)
         {
-            if (ExtendsContentIntoTitleBar == true)
-            {
-                // Set the initial interactive regions.
-                SetRegionsForCustomTitleBar();
-            }
+            // Set the initial interactive regions.
+            SetRegionsForCustomTitleBar();
         }
+    }
 
-    private void AppTitleBar_SizeChanged(object sender, SizeChangedEventArgs e)
+    void AppTitleBar_SizeChanged(object sender, SizeChangedEventArgs e)
+    {
+        if (ExtendsContentIntoTitleBar == true)
         {
-            if (ExtendsContentIntoTitleBar == true)
-            {
-                // Update interactive regions if the size of the window changes.
-                SetRegionsForCustomTitleBar();
-            }
+            // Update interactive regions if the size of the window changes.
+            SetRegionsForCustomTitleBar();
         }
+    }
 
-    private void SetRegionsForCustomTitleBar()
+    void SetRegionsForCustomTitleBar()
     {
         // Specify the interactive regions of the title bar.
 
         double scaleAdjustment = AppTitleBar.XamlRoot.RasterizationScale;
 
-        RightPaddingColumn.Width = new GridLength(m_AppWindow.TitleBar.RightInset / scaleAdjustment);
-        LeftPaddingColumn.Width = new GridLength(m_AppWindow.TitleBar.LeftInset / scaleAdjustment);
-
-        // Get the rectangle around the AutoSuggestBox control.
-        GeneralTransform transform = TitleBarSearchBox.TransformToVisual(null);
-        Rect bounds = transform.TransformBounds(new Rect(0, 0,
-                                                         TitleBarSearchBox.ActualWidth,
-                                                         TitleBarSearchBox.ActualHeight));
-        Windows.Graphics.RectInt32 SearchBoxRect = GetRect(bounds, scaleAdjustment);
-
-        // Get the rectangle around the PersonPicture control.
-        transform = PersonPic.TransformToVisual(null);
-        bounds = transform.TransformBounds(new Rect(0, 0,
-                                                    PersonPic.ActualWidth,
-                                                    PersonPic.ActualHeight));
-        Windows.Graphics.RectInt32 PersonPicRect = GetRect(bounds, scaleAdjustment);
-
-        var rectArray = new Windows.Graphics.RectInt32[] { SearchBoxRect, PersonPicRect };
-
-        InputNonClientPointerSource nonClientInputSrc =
-            InputNonClientPointerSource.GetForWindowId(this.AppWindow.Id);
-        nonClientInputSrc.SetRegionRects(NonClientRegionKind.Passthrough, rectArray);
+        RightPaddingColumn.Width = new GridLength(AppWindow.TitleBar.RightInset / scaleAdjustment);
+        LeftPaddingColumn.Width = new GridLength(AppWindow.TitleBar.LeftInset / scaleAdjustment);
     }
 
-    private Windows.Graphics.RectInt32 GetRect(Rect bounds, double scale)
-    {
-        return new Windows.Graphics.RectInt32(
-            _X: (int)Math.Round(bounds.X * scale),
-            _Y: (int)Math.Round(bounds.Y * scale),
-            _Width: (int)Math.Round(bounds.Width * scale),
-            _Height: (int)Math.Round(bounds.Height * scale)
-        );
-    }
 
-    private void MainWindow_Activated(object sender, WindowActivatedEventArgs args)
+    void MainWindow_Activated(object sender, WindowActivatedEventArgs args)
     {
         if (args.WindowActivationState == WindowActivationState.Deactivated)
         {
@@ -105,7 +75,7 @@ public sealed partial class MainWindow : Window
         }
     }
 
-    private void AppWindow_Changed(AppWindow sender, AppWindowChangedEventArgs args)
+    void AppWindow_Changed(AppWindow sender, AppWindowChangedEventArgs args)
     {
         if (args.DidPresenterChange)
         {
@@ -140,42 +110,8 @@ public sealed partial class MainWindow : Window
         }
     }
 
-    private void SwitchPresenter(object sender, RoutedEventArgs e)
+    void HamburgerButton_Click(object sender, RoutedEventArgs e)
     {
-        if (AppWindow != null)
-        {
-            AppWindowPresenterKind newPresenterKind;
-            switch ((sender as Button).Name)
-            {
-                case "CompactoverlaytBtn":
-                    newPresenterKind = AppWindowPresenterKind.CompactOverlay;
-                    break;
-
-                case "FullscreenBtn":
-                    newPresenterKind = AppWindowPresenterKind.FullScreen;
-                    break;
-
-                case "OverlappedBtn":
-                    newPresenterKind = AppWindowPresenterKind.Overlapped;
-                    break;
-
-                default:
-                    newPresenterKind = AppWindowPresenterKind.Default;
-                    break;
-            }
-
-            // If the same presenter button was pressed as the
-            // mode we're in, toggle the window back to Default.
-            if (newPresenterKind == AppWindow.Presenter.Kind)
-            {
-                AppWindow.SetPresenter(AppWindowPresenterKind.Default);
-            }
-            else
-            {
-                // Else request a presenter of the selected kind
-                // to be created and applied to the window.
-                AppWindow.SetPresenter(newPresenterKind);
-            }
-        }
+        
     }
 }
