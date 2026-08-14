f28e0700d9bb7312ec4d3556ba33c229f7cfba11
Uwe Riegel <uriegel@hotmail.de>
2026-08-14 23:38:28 +0200

    TODO

--- FileCommander/Controls/VirtualTable.xaml.cs
+++ FileCommander/Controls/VirtualTable.xaml.cs
@@ -26,6 +26,8 @@ namespace FileCommander.Controls;
 // TODO File SystemWatcher rename => delete create
 // TODO File SystemWatcher changed
 
+// TODO File SystemWatcher start exif and version
+
 // TODO File SystemWatcher Directory created
 // TODO File SystemWatcher Directory deleted
 // TODO File SystemWatcher Directory rename => delete create
